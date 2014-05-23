1ff2f838bbc0c7cecad064f499d85abb5cf3e0ab
Bruce Blank <Torsten.Blank@gmail.com>
2014-05-23 08:16:33 +0200

    Use more general class CCountingButtons for the toggle button

---
+++ CountingButtons.cpp
@@ -0,0 +1,75 @@
+#include <Arduino.h>
+#include "CountingButtons.h"
+
+// definition of the static values have to be done outside of the class
+unsigned int CCountingButtons::m_value0 = 0;
+unsigned int CCountingButtons::m_value1 = 0;
+unsigned int CCountingButtons::m_maxvalue0 = 1;
+unsigned int CCountingButtons::m_maxvalue1 = 1;
+
+CCountingButtons::CCountingButtons(int interruptID) : m_interruptID(interruptID)
+{
+	if(m_interruptID == 0){
+		pinMode(2, INPUT);
+		attachInterrupt(0, CCountingButtons::interruptFunction0, FALLING);
+	}else{
+		pinMode(3, INPUT);
+		attachInterrupt(1, CCountingButtons::interruptFunction1, FALLING);
+	}
+}
+
+void CCountingButtons::interruptFunction0()
+{
+	// increase value
+	if(++m_value0 > m_maxvalue0){
+		m_value0 = 0;
+	}
+}
+
+void CCountingButtons::interruptFunction1()
+{
+	// increase value
+	if(++m_value1 > m_maxvalue1){
+		m_value1 = 0;
+	}
+}
+
+void CCountingButtons::configButton0(unsigned int initValue, unsigned int maxValue)
+{
+	// set the static values, catching errors
+	m_maxvalue0 = (maxValue>0) ? maxValue : 1;
+	m_value0 = (initValue<=m_maxvalue0) ? initValue : 0 ;
+}
+
+void CCountingButtons::configButton1(unsigned int initValue, unsigned int maxValue)
+{
+	// set the static values, catching errors
+	m_maxvalue1 = (maxValue>1) ? maxValue : 1;
+	m_value1 = (initValue<=m_maxvalue1) ? initValue : 0 ;
+}
+
+
+CCountingButtons &CCountingButtons::instance0()
+{
+	static CCountingButtons instance(0);
+	return instance;
+}
+
+CCountingButtons &CCountingButtons::instance1()
+{
+	static CCountingButtons instance(1);
+	return instance;
+}
+
+unsigned int CCountingButtons::getValue()
+{
+	if(m_interruptID ==0)
+		return m_value0;
+	else
+		return m_value1;
+}
+
+
+
+
+
---
+++ CountingButtons.h
@@ -0,0 +1,40 @@
+// a singleton button class: it counts how often the button has been clicked.
+// When a configurable maximum value will be reached, the counter will be reset to 0
+class CCountingButtons{
+private:
+	const int m_interruptID;
+
+	static unsigned int m_value0;
+	static unsigned int m_value1;
+	static unsigned int m_maxvalue0;
+	static unsigned int m_maxvalue1;
+
+	// direct construction is private
+	CCountingButtons();
+	// copy construction is private
+	CCountingButtons(const CCountingButtons &);
+	// direct construction with parameter is private
+	// interruptID is 0 (button on PIN2) or 1 (button on PIN3)
+	CCountingButtons(int interruptID);
+
+	// interrupt function increases value of button 0 up to a maximum value
+	static void interruptFunction0();
+	// interrupt function increases value of button 1 up to a maximum value
+	static void interruptFunction1();
+
+public:
+	// define the initial and maximum value of the button 0
+	static void configButton0(unsigned int initValue = 0, unsigned int maxValue = 1);
+	// define the initial and maximum value of the button 1
+	static void configButton1(unsigned int initValue = 0, unsigned int maxValue = 1);
+
+	// get instance for button with interrupt-ID 0 (PIN 2)
+	static CCountingButtons &instance0();
+	// get instance for button with interrupt-ID 1 (PIN 3)
+	static CCountingButtons &instance1();
+
+	// get the value of the current button instance
+	unsigned int getValue();
+};
+
+
